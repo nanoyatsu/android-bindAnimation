89de7fdf60d97641c7af649c01aeef84fa6ba9f3
nanoyatsu <34255833+nredjap@users.noreply.github.com>
2020-09-27 21:26:15 +0900

    カウントまで

--- app/src/main/java/com/nanoyatsu/example/bindAnimation/ui/main/MainFragment.kt
+++ app/src/main/java/com/nanoyatsu/example/bindAnimation/ui/main/MainFragment.kt
@@ -1,14 +1,15 @@
 package com.nanoyatsu.example.bindAnimation.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nanoyatsu.example.bindAnimation.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nanoyatsu.example.bindAnimation.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,15 +17,20 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
 }
--- app/src/main/java/com/nanoyatsu/example/bindAnimation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nanoyatsu/example/bindAnimation/ui/main/MainViewModel.kt
@@ -1,7 +1,14 @@
 package com.nanoyatsu.example.bindAnimation.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _qty = MutableLiveData(0)
+    val qty: LiveData<Int> = _qty
+
+    fun qtyPlus() = _qty.postValue(_qty.value!! + 1)
+    fun qtyMinus() = _qty.postValue(_qty.value!! - 1)
+
 }
